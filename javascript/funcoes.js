29f5156d3044e2b33a035659b7be81a408f990e0
Bruno A <bruno.souza2001@gmail.com>
2023-03-30 16:55:12 -0300

    Validacoes no Javascript da tela de cadastro e ajustes na home

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,1 +1,26 @@
-console.log("Hellow World");
+function mascara(i) {
+
+    var v = i.value;
+
+    if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
+        i.value = v.substring(0, v.length - 1);
+        return;
+    }
+
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+}
+
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g, '')
+    value = value.replace(/(\d{2})(\d)/, "($1) $2")
+    value = value.replace(/(\d)(\d{4})$/, "$1-$2")
+    return value
+}
