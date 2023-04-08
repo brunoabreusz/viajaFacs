f97f47baf282672e00ac620a53435b84dd512e14
Bruno Abreu <bruno.souza2001@gmail.com>
2023-04-07 23:46:28 -0300

    Alterações na Home, Estilização da tela de passagens, Validação na tela de cadastro

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,26 +1,57 @@
 function mascara(i) {
 
-    var v = i.value;
+  var v = i.value;
 
-    if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
-        i.value = v.substring(0, v.length - 1);
-        return;
-    }
+  if (isNaN(v[v.length - 1])) { // impede entrar outro caractere que não seja número
+    i.value = v.substring(0, v.length - 1);
+    return;
+  }
 
-    i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += ".";
-    if (v.length == 11) i.value += "-";
+  i.setAttribute("maxlength", "14");
+  if (v.length == 3 || v.length == 7) i.value += ".";
+  if (v.length == 11) i.value += "-";
 }
 
 const handlePhone = (event) => {
-    let input = event.target
-    input.value = phoneMask(input.value)
+  let input = event.target
+  input.value = phoneMask(input.value)
 }
 
 const phoneMask = (value) => {
-    if (!value) return ""
-    value = value.replace(/\D/g, '')
-    value = value.replace(/(\d{2})(\d)/, "($1) $2")
-    value = value.replace(/(\d)(\d{4})$/, "$1-$2")
-    return value
+  if (!value) return ""
+  value = value.replace(/\D/g, '')
+  value = value.replace(/(\d{2})(\d)/, "($1) $2")
+  value = value.replace(/(\d)(\d{4})$/, "$1-$2")
+  return value
 }
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+
+
+function checkPasswords() {
+  var password = document.forms["cadastro"]["password"].value;
+  var conPassword = document.forms["cadastro"]["con-password"].value;
+
+  if (password !== conPassword) {
+    alert("As senhas não coincidem. Por favor, tente novamente.");
+    return false;
+  }
+  
+  alert("Cadastro concluido.");
+
+  setTimeout(() => {
+    window.location.href = 'login.html';
+  }, 10);
+  
+  return true;
+}
+
